6ddf740280d5ee572d5ba9ae05c63d6cb3b9d1af
Avinash <avinash@unisasitbiz.com>
2026-01-22 15:19:06 +0530

    technical2

--- biometric_integration/models/hr_employee.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class HrEmployee(models.Model):
-    """Inherit the model to add field"""
-    _inherit = 'hr.employee'
-
-    device_id_num = fields.Char(string='Biometric Device ID',
-                                help="Give the biometric device id")
